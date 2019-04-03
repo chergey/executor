2b7f4d7e348d8713594631e4e5790037ffd1e229
cossack5 <cossack5@mail.ru>
2019-04-04 01:37:23 +0300

    initial commit

---
+++ src/test/java/org/elcer/AppTest.java
@@ -0,0 +1,42 @@
+package org.elcer;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+
+    @Test
+    public void testExecutor() throws InterruptedException {
+
+        Executor executor = new Executor();
+
+        List<Integer> executionOrder = new CopyOnWriteArrayList<>();
+
+        executor.execute(LocalDateTime.now(), () -> executionOrder.add(1));
+        executor.execute(LocalDateTime.now().plusSeconds(3), () -> executionOrder.add(2));
+        executor.execute(LocalDateTime.now().plusSeconds(3), () -> executionOrder.add(3));
+        executor.execute(LocalDateTime.now().plusSeconds(6), () -> executionOrder.add(4));
+        executor.execute(LocalDateTime.now().plusSeconds(6), () -> executionOrder.add(5));
+        executor.execute(LocalDateTime.now().plusSeconds(1), () -> executionOrder.add(6));
+
+        Thread.sleep(10000);
+
+        executor.stop();
+
+        int[] orderNumbers = executionOrder.stream().mapToInt(o -> o).toArray();
+
+        Assert.assertArrayEquals( new int[] {1, 6, 2,3, 4, 5}, orderNumbers);
+
+    }
+}
