5e60c0ddc04e25f089bf00350235d3321cde5151
cossack5 <cossack5@mail.ru>
2019-04-05 23:18:20 +0300

    test for submission time based execution

--- src/test/java/org/elcer/AppTest.java
+++ src/test/java/org/elcer/AppTest.java
@@ -25,13 +25,21 @@ public class AppTest {
         executor.execute(LocalDateTime.now().plusSeconds(6), () -> executionOrder.add(5));
         executor.execute(LocalDateTime.now().plusSeconds(1), () -> executionOrder.add(6));
 
+        executor.execute(LocalDateTime.now().plusNanos(1), () -> executionOrder.add(7));
+        executor.execute(LocalDateTime.now().plusNanos(2), () -> executionOrder.add(8));
+        executor.execute(LocalDateTime.now().plusNanos(3), () -> executionOrder.add(9));
+
+        executor.execute(LocalDateTime.now().plusNanos(3), () -> executionOrder.add(10));
+        executor.execute(LocalDateTime.now().plusNanos(3), () -> executionOrder.add(11));
+        executor.execute(LocalDateTime.now().plusNanos(3), () -> executionOrder.add(12));
+
         Thread.sleep(10000);
 
         executor.stop();
 
         int[] orderNumbers = executionOrder.stream().mapToInt(o -> o).toArray();
 
-        Assert.assertArrayEquals( new int[] {1, 6, 2,3, 4, 5}, orderNumbers);
+        Assert.assertArrayEquals(new int[]{1, 7, 8, 9, 10, 11, 12, 6, 2, 3, 4, 5}, orderNumbers);
 
     }
 }
